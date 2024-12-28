087e3923ad935868b16762cce51aa2e5f5f253b2
yuqiu2004 <2506852507@qq.com>
2024-12-28 09:38:04 +0800

    fix : delete unused api

--- job-predict-ui/src/api/index.js
+++ job-predict-ui/src/api/index.js
@@ -3,15 +3,3 @@ import request from '../utils/request'
 export const login = (data) => {
     return request.post('/user/login', data)
 }
-
-export const uploadSoldierMsg = (data) => {
-    return request.post('/soldier', data)
-}
-
-export const getSoldierList = (params) => {
-    return request.get('/soldier/list', {params})
-}
-
-export const getSoldierById = (id) => {
-    return request.get('/soldier/detail', { params: { id } });
-}
